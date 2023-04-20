3bec5707126501bb548d223b42b1ee81b23da2f1
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-19 21:26:43 -0300

    Adicionar arquivos de projeto.

---
+++ [M2S03]--DevInHouse.Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _M2S03___DevInHouse.Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Somar(5,6));
+
+
+
+            
+
+        }
+        public static int Somar(int x, int y)
+        {
+            return x + y;
+        }
+
+
+    }
+}
---
+++ [M2S03]--DevInHouse.Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _M2S03___DevInHouse.Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Somar(5, 6));
+            Console.WriteLine(Somar(5, 6,10));
+
+
+
+
+        }
+        public static int Somar(double x, double y)
+        {
+            return (int)(x + y);
+        }
+
+        public static int Somar(double x, double y, double z)
+        {
+            return (int)(x + y + z);
+        }
+    }
+}
