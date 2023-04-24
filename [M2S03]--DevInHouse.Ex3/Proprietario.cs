db55a4a91f87144c4506cceb460a3e6df29fe0d6
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-24 19:12:43 -0300

    Exercicio 3

---
+++ [M2S03]--DevInHouse.Ex3/Proprietario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S03___DevInHouse.Ex3
+{
+    public  class Proprietario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+    }
+}
