0463860b9fd0e7560e75ee2ec8df734d466e4da8
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-24 19:23:46 -0300

    Exercicio 4

--- [M2S03]--DevInHouse.Ex3/Program.cs
+++ [M2S03]--DevInHouse.Ex3/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Proprietario proprietario1 = new();
+            Proprietario proprietario2 = new Proprietario();
+            Proprietario proprietario3 = new Proprietario();
+
+            proprietario1.Nome = "Joao";
+            proprietario1.Cpf = "344.555.444.05";
+            proprietario1.Telefone = "99-3443-3234";
+
+            proprietario2.Nome = "Pedrao";
+            proprietario2.Cpf = "224.545.432.33";
+            proprietario2.Telefone = "44-332-2334";
+
+            proprietario3.Nome = "Marlene";
+            proprietario3.Cpf = "234.678.545.23";
+            proprietario3.Telefone = "65-4223-3234";
+
         }
     }
 }
