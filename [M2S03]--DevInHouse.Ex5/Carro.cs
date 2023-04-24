fb78bee42af1ad733542a4519c45368be0b0dd5b
Anderson Braga Oliveira <anderson_braga_oliveira@hotmail.com>
2023-04-24 19:29:03 -0300

    Exercicio 5

---
+++ [M2S03]--DevInHouse.Ex5/Carro.cs
@@ -0,0 +1,18 @@
+﻿using _M2S03___DevInHouse.Ex3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _M2S03___DevInHouse.Ex5
+{
+    public class Carro
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public Proprietario Proprietario { get; set; }
+    }
+}
---
+++ [M2S03]--DevInHouse.Ex5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _M2S03___DevInHouse.Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Proprietario proprietario1 = new();
+            Proprietario proprietario2 = new Proprietario();
+            Proprietario proprietario3 = new Proprietario();
+
+            proprietario1.Nome = "Joao";
+            proprietario1.Cpf = "344.555.444.05";
+            proprietario1.Telefone = "99-3443-3234";
+
+            proprietario2.Nome = "Pedrao";
+            proprietario2.Cpf = "224.545.432.33";
+            proprietario2.Telefone = "44-332-2334";
+
+            proprietario3.Nome = "Marlene";
+            proprietario3.Cpf = "234.678.545.23";
+            proprietario3.Telefone = "65-4223-3234";
+
+        }
+    }
+}
